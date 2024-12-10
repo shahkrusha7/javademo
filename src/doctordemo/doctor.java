02b47476e12373b2627a65cc1637b2f8b5e14ab9
shahkrusha7 <krusha.shah2810@gmail.com>
2024-12-10 22:02:51 +0530

    another option to search by disease name added

--- src/doctordemo/doctor.java
+++ src/doctordemo/doctor.java
@@ -8,22 +8,35 @@ public class doctor extends patientdetails
     String docname;
     int fees;
 
-    //ArrayList<patientdetails> patients=new ArrayList<>();
+    ArrayList<patientdetails> patients=new ArrayList<>();
 
     void setdoctor()
     {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter Doctor's Name: ");
         docname=sc.next();
-        setpatientdetails();
         System.out.print("Enter Consultancy Fees: ");
         fees=sc.nextInt();
+
+        String option="y";
+        do
+        {
+            patientdetails patient =new patientdetails();
+            patient.setpatientdetails();
+            System.out.print("Do you want to add another patient? (y/n): ");
+            option=sc.next();
+            patients.add(patient);
+        }while (option.equalsIgnoreCase("y"));
     }
 
     void getdoctor()
     {
         System.out.println("Doctor's Name: "+docname);
-        getpatientdetails();
         System.out.println("Consultancy Fees: "+fees);
+
+        for(patientdetails patient:patients)
+        {
+            patient.getpatientdetails();
+        }
     }
 }
--- src/doctordemo/doctormain.java
+++ src/doctordemo/doctormain.java
@@ -1,5 +1,7 @@
 package doctordemo;
 
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class doctormain
@@ -7,15 +9,18 @@ public class doctormain
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
+        ArrayList<doctor> doctors=new ArrayList<>();
         int option1=0;
-        doctor doctors=new doctor();
+        boolean found=false;
+        doctor doctor=new doctor();
 
         do
         {
             System.out.println("\n\t Patient's Details");
             System.out.println("1. Entry");
             System.out.println("2. View");
-            System.out.println("3.  Exit");
+            System.out.println("3. Search by Disease's Name:");
+            System.out.println("4.  Exit");
 
             System.out.print("Enter Your option: ");
             option1=sc.nextInt();
@@ -24,15 +29,36 @@ public class doctormain
             {
                 case 1:
                     System.out.println("\n\t Patient's Entry ");
-                    doctors.setdoctor();
+                    doctor.setdoctor();
+                    doctors.add(doctor);
                 break;
 
                 case 2:
                     System.out.println("\n\t Patient View");
-                    doctors.getdoctor();
+                    doctor.getdoctor();
+                    System.out.println("----------------------------------");
                 break;
 
                 case 3:
+                    System.out.println("\n\t Search by disease's name");
+                    System.out.print("Enter disease name: ");
+                    String searchdisease=sc.next();
+
+                    for(doctor d:doctors)
+                    {
+                        for(patientdetails p:d.patients)
+                            if(p.disease.equalsIgnoreCase(searchdisease))
+                            {
+                                d.getdoctor();
+                                found=true;
+                                break;
+                            }
+                    }
+                    if(!found)
+                        System.out.println("No record of mentioned disease found");
+                break;
+
+                case 4:
                     System.out.println("You are exited");
                 break;
 
@@ -40,6 +66,6 @@ public class doctormain
                     System.out.println("Wrong option selected try again");
                 break;
             }
-        }while (option1!=3);
+        }while (option1!=4);
     }
 }
